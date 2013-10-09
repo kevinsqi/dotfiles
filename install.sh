27a261859f79d6e773c967bf3c2c52d337cb48ce
Kevin Qi <iqnivek@gmail.com>
2013-10-09 02:00:46 -0400

    Add initial version of install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# TODO not yet tested
+
+mv ~/.bashrc ~/.bashrc.bak.`date +%Y%m%d%H%M%S`
+mv ~/.vimrc ~/.vimrc.bak.`date +%Y%m%d%H%M%S`
+
+DOTFILE_DIR="$( cd "$( dirname "$0" )" && pwd )"
+ln -s $DOTFILE_DIR/.bashrc ~/.bashrc
+ln -s $DOTFILE_DIR/.vimrc ~/.vimrc
+
+git clone https://github.com/gmarik/vundle.git /.vim/bundle/vundle
+vim +BundleInstall +qall
