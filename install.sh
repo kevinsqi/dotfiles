f1c693fee1fe8c2d748e352ddb90ae758bb5c8ca
Kevin Qi <iqnivek@gmail.com>
2020-02-26 21:20:01 -0800

    revamp install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#
+# symlink dotfiles
+#
+
 # TODO conditional on if .bashrc/.vimrc are files
 mv ~/.bashrc ~/.bashrc.bak.`date +%Y%m%d%H%M%S`
 mv ~/.bash_profile ~/.bash_profile.bak.`date +%Y%m%d%H%M%S`
@@ -5,28 +9,39 @@ mv ~/.vimrc ~/.vimrc.bak.`date +%Y%m%d%H%M%S`
 
 DOTFILE_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# symlink synced files
 ln -s $DOTFILE_DIR/.bashrc ~/.bashrc
 ln -s $DOTFILE_DIR/.bash_profile ~/.bash_profile
 ln -s $DOTFILE_DIR/.vimrc ~/.vimrc
 ln -s $DOTFILE_DIR/.gitignore_global ~/.gitignore_global
 
-# mac-specific (TODO make conditional)
-ln -s $DOTFILE_DIR/.bashrc_osx ~/.bashrc_osx
-ln -s $DOTFILE_DIR/.git-completion.bash ~/.git-completion.bash
-
 # z.sh
 ln -s $DOTFILE_DIR/z.sh ~/z.sh
 
-# git stuff
+
+#
+# mac preferences
+#
+
+ln -s $DOTFILE_DIR/.bashrc_osx ~/.bashrc_osx
+ln -s $DOTFILE_DIR/.git-completion.bash ~/.git-completion.bash
+
+# show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles YES && killall Finder
+
+
+#
+# git
+#
+
 cp $DOTFILE_DIR/.gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore_global
 
-# upgrade vim to vim8
-brew install vim --with-override-system-vi
+#
+# vim
+#
 
-# install black plugin with pip
-pip3 install black
+# upgrade vim to vim8
+brew install vim
 
 # install vim plugins
 if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
@@ -34,7 +49,10 @@ if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
     vim +BundleInstall +qall
 fi
 
-# install homebrew packages
+#
+# homebrew packages
+#
+
 brew install \
   fd \
   fzf \
@@ -48,6 +66,9 @@ brew install \
 $(brew --prefix)/opt/fzf/install
 
 # optional, long homebrew packages
-brew install postgresql
-brew install awscli
-brew install terraform
+# brew install postgresql
+# brew install awscli
+# brew install terraform
+
+# install black plugin with pip
+# pip3 install black
